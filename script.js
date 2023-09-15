7f7dcf3e371143870297b345a8dbb96430341d0e
Vishal Bharti <100204338+Usecodevishal@users.noreply.github.com>
2023-09-15 23:09:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+   let formTag = document.getElementById("form1");
+	let firstN = formTag.children[0].value;
+	let lastN = formTag.children[2].value;
+	alert(`${firstN} ${lastN}`);
 }
